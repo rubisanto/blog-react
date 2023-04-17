61cfd3541c40d67c5da563df4e52fe86b6f1d58b
rubisanto <rubisanto06@gmail.com>
2023-04-17 14:24:53 +0200

    folders init

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
+
+export default function Navbar() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [width, setWidth] = useState(window.innerWidth);
+
+  const toggleNav = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", changeWidth);
+    // clean up function
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+
+  return (
+    <div>
+      <nav>
+        {(toggleMenu || width > 500) && (
+          <ul className="liste">
+            {/* 5 li with class items "accueil" "service" "contact" */}
+            <li className="items">Accueil</li>
+            <li className="items">Services</li>
+            <li className="items">Contact</li>
+          </ul>
+        )}
+        <button onClick={toggleNav} className="btn">
+          BTN
+        </button>
+      </nav>
+    </div>
+  );
+}
